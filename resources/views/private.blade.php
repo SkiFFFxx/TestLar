cd9082357ab6642f330514f82aafaab0ad911317
SkiFFFxx <kim_lev12@mail.ru>
2023-06-04 14:42:12 +0600

    bc auth/reg

---
+++ resources/views/private.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h>This is private page</h>
+</body>
+<a class="me-3 py-2 link-body-emphasis text-white" href="/logout">logout</a>
+</html>
+
