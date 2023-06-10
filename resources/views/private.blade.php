40a8d8027beed5c116007071b3c537bca997d8ad
SkiFFFxx <kim_lev12@mail.ru>
2023-06-10 12:10:23 +0600

    bc articles/styles for pages

--- resources/views/private.blade.php
+++ resources/views/private.blade.php
@@ -1,31 +1,27 @@
 @extends('/dry')
 
+@section('title-block')
+    Private Page
+@endsection
+
+@section('main_content')
+
+    <h1 class="text-center mt-5">This is private page</h1>
+
+    <br>
+
+    <div class="d-flex justify-content-center">
+        <a class="btn btn-primary me-3" href="/logout">Сменить аккаунт</a>
+        @can('view-admin-panel')
+            <a class="btn btn-danger me-3" href="/inner">Панель администратора</a>
+
+        @endcan
 
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-<h1 class="text-center mt-5">This is private page</h1>
-
-<br>
-
-<div class="d-flex justify-content-center">
-    <a class="btn btn-primary me-3" href="/logout">Сменить аккаунт</a>
-    @can('view-admin-panel')
-    <a class="btn btn-danger me-3" href="/inner">Панель администратора</a>
-
-    @endcan
+    </div>
 
 
-</div>
-</body>
-</html>
+@endsection
+
+
 
